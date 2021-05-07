f9b86bfcf2273abfc43c833cbede47fd37883c39
DenisWritesCode <deniswritescode@gmail.com>
2021-05-07 21:49:58 +0200

    Added Layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import React from "react";
+
+import Overview from "./Components/Overview";
+
+class Form extends React.Component {
+  render() {
+    return (
+      <div className="container ">
+        <div className="form">
+          <form action="#">
+            <label htmlFor="task">
+              <input type="text" name="task" id="task" />
+            </label>
+
+            <button type="submit">Add Task</button>
+          </form>
+        </div>
+        <Overview />
+      </div>
+    );
+  }
+}
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Overview.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Overview extends React.Component {
+  render() {
+    return (
+      <div className="list">
+        <ul>
+          <li>Task 1</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Overview;
